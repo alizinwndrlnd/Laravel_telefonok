dfb10e44b4b369488b26ac13b393cbe2553292a0
neu <aliz.encsy@gmail.com>
2024-11-07 18:55:15 +0000

    PhoneController

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PhoneController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [PhoneController::class,"index"] );
+
